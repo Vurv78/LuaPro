ca9b8ab28109d4dea0655355f7be4e08175dd1fb
Vurv <56230599+Vurv78@users.noreply.github.com>
2022-06-09 21:31:04 -0700

    Use ubuntu for tests

--- test/main.lua
+++ test/main.lua
@@ -1,7 +1,7 @@
 ---@param path string
 ---@param callback fun(path: string)
 local function iterFiles(path, callback)
-	local dir = io.popen("dir /b /a " .. path)
+	local dir = io.popen("ls " .. path)
 	for file in dir:lines() do
 		callback(file)
 	end
@@ -9,8 +9,8 @@ local function iterFiles(path, callback)
 end
 
 local failures = 0
-iterFiles("test\\cases", function(path)
-	local handle = assert( io.open("test\\cases\\" .. path, "r"), "Failed to open file: " .. path)
+iterFiles("test/cases", function(path)
+	local handle = assert( io.open("test/cases/" .. path, "r"), "Failed to open file: " .. path)
 	local code = handle:read("*a")
 
 	local ok = pcall( loadstring(code, "test case " .. path) )
