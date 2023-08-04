a078272b5b62a0155b6deaa44e9162394f6e1c9d
Vurv <56230599+Vurv78@users.noreply.github.com>
2023-08-04 11:20:28 -0700

    Reimplement identifier restriction but also allow other nodes
    Not correct to just allow anything since lua doesn't parse stuff like `2()`. It does seem to allow idents, indexing operations, calls and grouped expressions. This seems to be good enough

--- src/lupa.lua
+++ src/lupa.lua
@@ -440,7 +440,7 @@ local function parse(tokens, version)
 				local method = assert(consume(TokenVariant.Identifier), "Expected method name after :")
 				assert(consume(TokenVariant.Grammar, "("), "Expected ( to start methodcall arguments")
 				p = Node.new(NodeVariant.MethodCall, { p, method, arguments("method call", ")") })
-			elseif p then
+			elseif p and (p.variant == NodeVariant.Identifier or p.variant == NodeVariant.Index or p.variant == NodeVariant.Call or p.variant == NodeVariant.Grouped) then
 				local str = consume(TokenVariant.String) or consume(TokenVariant.MString)
 				if str then
 					p = Node.new(NodeVariant.Call, { p, { Node.new(NodeVariant.Literal, { str.variant, str }) } })
